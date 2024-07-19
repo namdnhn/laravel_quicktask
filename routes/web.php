1151a427b51c9086efdc3b31e13bdb3983c9d8a3
Do Nhu Hoang Nam <114157252+namdnhn@users.noreply.github.com>
2024-07-19 15:23:02 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\TaskController;
 
-Auth::loginUsingId(2);
 Route::get('/', function () {
     return view('welcome');
 });
